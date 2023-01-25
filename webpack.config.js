127dc4b19cfcdb845080281a4639ed3e70f7ce91
eakray <ekrayni@gmail.com>
2023-01-24 21:55:54 -0600

    further yarn cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,58 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/client/index.jsx",
-  devtool: "inline-source-map",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: "./src/client/index.html",
-    }),
-  ],
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, "src/client/*"),
+    entry: "./src/client/index.jsx",
+    devtool: "inline-source-map",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
     },
-    extensions: [".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-            ],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, "src/client"),
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/client/index.html"
+        })
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
+    resolve: {
+        fallback: {
+            fs: false,
+            tls: false,
+            net: false,
+            path: false,
+            zlib: false,
+            http: false,
+            https: false,
+            stream: false,
+            crypto: false,
+            vm: false
+        },
+        alias: {
+            components: path.resolve(__dirname, "src/client/*")
+        },
+        extensions: [".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            ["@babel/preset-react", { runtime: "automatic" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, "src/client"),
+                use: ["style-loader", "css-loader", "postcss-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    }
 };
